93d195b53297674500a12c5c3fd7a17dc8f4eb38
LebronXia <529330360@qq.com>
2025-07-14 11:23:04 +0800

    feat: 创建4个module

---
+++ feature/feature-profile/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.wanandroid.android.feature)
+    alias(libs.plugins.wanandroid.android.library.compose)
+}
+
+android {
+    namespace = "com.riane.feature.profile"
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(project(":data:repository"))
+    implementation(project(":domain:domain-auth"))
+}
---
+++ feature/feature_system/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.wanandroid.android.feature)
+    alias(libs.plugins.wanandroid.android.library.compose)
+}
+
+android {
+    namespace = "com.riane.feature.system"
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(project(":data:repository"))
+    implementation(project(":domain:domain-auth"))
+}
